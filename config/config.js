44db9acf49235f2c4246ad665f3ea356375facf7
Ruby Tech <reubenalabi2006@gmail.com>
2024-08-04 20:40:19 +0100

    postgres 3

--- config/config.js
+++ config/config.js
@@ -20,7 +20,7 @@ export default {
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: "share-test",
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "postgres",
   },
