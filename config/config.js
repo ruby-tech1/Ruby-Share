5f5ac1f1adbdf1ca1f09bc7ad9e5330713b42ec1
Ruby Tech <reubenalabi2006@gmail.com>
2024-08-04 21:15:45 +0100

    postgres 4

--- config/config.js
+++ config/config.js
@@ -3,26 +3,16 @@ dotenv.config();
 
 export default {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
     dialect: "postgres",
     logging: false,
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: "share-test",
-    host: process.env.DB_HOST,
     dialect: "postgres",
+    logging: false,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
     dialect: "postgres",
+    logging: false,
   },
   // production: {
   //   url: "postgresql://test_dev_5ssc_user:bCaUQwFn0N5ZPXdfra6taf4vPSlAPeEz@dpg-cqn69slsvqrc73fk8cvg-a.oregon-postgres.render.com/test_dev_5ssc",
@@ -36,3 +26,11 @@ export default {
   //   },
   // },
 };
+
+// {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: "share-test",
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//   },
--- config/connectDb.js
+++ config/connectDb.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
 const env = process.env.NODE_ENV || "development";
 import config from "./config.js";
 
-const sequelize = new Sequelize(config[env]);
+const sequelize = new Sequelize(process.env.DB_URL, config[env]);
 
 export default sequelize;
